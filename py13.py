ea93785329b23eeb968f4cd2a8455f6f2c4e629e
Pratiksahu-02 <pratiksahu1802@gmail.com>
2024-10-30 19:19:22 -0400

    new update

--- py13.py
+++ py13.py
@@ -7,3 +7,4 @@ a.append(l[1])
 print(l)
 a.extend(l)
 print(a)
+# there is more to update 
--- py9.py
+++ py9.py
@@ -1,2 +1,17 @@
 # Given three points (x1, y1), (x2, y2), (x3, y3),
 # write a program to check all the three points fall on one straight line.
+x1=int(input("Enter the x1 point:"))
+x2=int(input("Enter the x2 point:"))
+x3=int(input("Enter the x3 point:"))
+y1=int(input("Enter the y1 point:"))
+y2=int(input("Enter the y2 point:"))
+y3=int(input("Enter the y3 point:"))
+
+def straight_line(x1, y1, x2, y2, x3, y3):
+    
+    return (x2 - x1) * (y3 - y1) == (y2 - y1) * (x3 - x1)
+
+if straight_line(x1, y1, x2, y2, x3, y3):
+    print("The points are straight_line.")
+else:
+    print("The points are not straight_line.")
