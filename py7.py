4ed3156e99949457cec70cc973b9d15db794ff6a
Pratiksahu-02 <pratiksahu1802@gmail.com>
2024-10-29 20:08:09 -0400

    new update

---
+++ py7.py
@@ -0,0 +1,6 @@
+# WAP to find out whteher the integer entered by user , through the keybord ,is even or odd number
+a=int(input("Enter a number:"))
+if(a%2==0):
+    print(a,"is even number.")
+else:
+    print(a,"is odd number.")
