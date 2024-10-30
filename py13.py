d987d3ff4fddaf0eb910ecfeb268e59e12499649
Pratiksahu-02 <pratiksahu1802@gmail.com>
2024-10-30 17:57:53 -0400

    new program

---
+++ py13.py
@@ -0,0 +1,9 @@
+# Write a program to demonstrate basic operations on the list.
+a=["apple","orange","cherry","banana"]
+print(len(a))
+print(a.pop())
+l=["1","2","3"]
+a.append(l[1])
+print(l)
+a.extend(l)
+print(a)
---
+++ py9.py
@@ -0,0 +1,2 @@
+# Given three points (x1, y1), (x2, y2), (x3, y3),
+# write a program to check all the three points fall on one straight line.
