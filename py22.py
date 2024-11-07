fafe5b46d7a09bcc1c70a072d6d93c9e1bd1d839
Pratiksahu-02 <pratiksahu1802@gmail.com>
2024-11-07 10:01:45 -0500

    ok

---
+++ py22.py
@@ -0,0 +1,5 @@
+#Create a dictionary to store data (name, roll number) of N students. 
+#The key will be the roll number of the student and the value contains the 
+#data of the student (in a list). Write a program that asks the user to enter 
+#a name of a Student, search it in the dictionary and print the data of the 
+#Student if it is available otherwise display an appropriate message.
