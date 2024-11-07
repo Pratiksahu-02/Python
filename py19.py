3fb4fdc7b2762651241ede8dea63a698b87cede8
Pratiksahu-02 <pratiksahu1802@gmail.com>
2024-11-07 07:19:38 -0500

    ok

--- py19.py
+++ py19.py
@@ -1,1 +1,16 @@
-#Write a program to demonstrate basic operations on the set.
+#WAP to demonstrate basic operations on the set.
+
+a={}
+s={}
+print(type(s))
+print()
+for i in a:
+    print(i)
+print(s.update())
+print(a.remove("l"))
+a.discard()
+#print(a)
+#union()
+#intersection()
+#difference()
+#symmetric_differece()
