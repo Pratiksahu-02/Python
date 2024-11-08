0ebbd04c431bf00c6bc9523b48dffc1dafa7701d
Pratiksahu-02 <pratiksahu1802@gmail.com>
2024-11-08 07:47:23 -0500

    ok

---
+++ program/py21.py
@@ -0,0 +1,18 @@
+#Write a program to demonstrate basic operations on the dictionary.
+
+d={}
+print(len(d))
+print(d.get())
+print(d.items())
+print(d.keys())
+print(d.values())
+print(d.popitem())
+print(d.min())
+print(d.max())
+print(d.sort())
+print(d.pop())
+print(d.update())
+print(d.shorted())
+print(d.fromkeys())
+print(d.setdefault("Ok right"))
+print(d.clear())
