e15498027fb1ed80e55c0a7d546a2bf120578698
Pratiksahu-02 <pratiksahu1802@gmail.com>
2024-11-29 19:57:57 +0530

    ok

--- program/py22.py
+++ program/py22.py
@@ -3,3 +3,21 @@
 #data of the student (in a list). Write a program that asks the user to enter 
 #a name of a Student, search it in the dictionary and print the data of the 
 #Student if it is available otherwise display an appropriate message.
+
+d={"roll_number":["name","class","Age","hobby"]}
+a=int(input("How many student data u want to enter:"))
+for i in range (a):
+    roll=int(input("Enter roll no.:"))
+    name=input("Name of the student:")
+    c=input("Enter class:")
+    age=int(input("Enter age of the student:"))
+    h=input("Enter hobby:")
+    d[roll]=[name,c,age,h]
+    print()
+
+s=input("Enter name of the student ")
+for x,j in d:
+    if (j[0]==s):
+        print(x,j,sep=":")
+
+#incomplet 
