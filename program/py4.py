57515a8a7dbf6c3c02d8d719bf1daba98a5f81ee
Pratiksahu-02 <pratiksahu1802@gmail.com>
2024-11-29 19:26:05 +0530

    ok

--- program/py4.py
+++ program/py4.py
@@ -1,3 +1,17 @@
 #WAP that will find the roots of a quaduratic equation:ax^2+bx+c=0
  
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+c=int(input("Enter the value of c:"))
 
+d=(b*b)-4*a*c
+
+if(d>0):
+    r1=(-b+(d**0.5))/(2*a)
+    r2=(-b-(d**0.5))/(2*a)
+    print("The roots of the equation are:",r1,"and",r2)
+elif(d==0):
+    r1=(-b+(d**0.5))/(2*a)
+    print("The roots of the equation are:",r1)
+else:
+    print("The roots of the equation are imaginary")
