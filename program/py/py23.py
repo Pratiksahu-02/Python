d790c0aa934ec4a6351c984b5cfa36fb55ba88ec
Pratiksahu-02 <pratiksahu1802@gmail.com>
2025-01-08 17:27:27 +0530

    new

--- program/py/py23.py
+++ program/py/py23.py
@@ -1,2 +1,20 @@
 #Write a program to demonstrate basic 
 #comprehensions on list, set and dictionary.
+
+#List comprehension
+#list are mutable and ordered collection of items.
+#List comprehension is an elegant way to define and create list in python.
+a=[x for x in range(10)]
+print(a)
+
+#Set comprehension
+#set is an unordered collection of unique items.
+#Set comprehension is an elegant way to define and create set in python.
+b={x for x in range(10)}
+print(b)
+
+#Dictionary comprehension
+#Dictionary is an unordered collection of key-value pairs.
+#Dictionary comprehension is an elegant way to define and create dictionary in python.
+c={x:x*x for x in range(10)}
+print(c)
