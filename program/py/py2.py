126ce3b065850da54b6383b4916676aa42caa665
Pratiksahu-02 <pratiksahu1802@gmail.com>
2025-01-10 14:49:21 +0530

    ok

--- program/py/py2.py
+++ program/py/py2.py
@@ -14,4 +14,27 @@ if(a==1):
     r=int(input("Enter a nnumber:"))
     print(sqrt(r))
 elif(a==2):
+    x = int(input("Enter the base number: "))
+    y = int(input("Enter the exponent: "))
+    print(pow(x, y))
+elif(a==3):
+    x = float(input("Enter a number: "))
+    print(ceil(x))
+elif(a==4):
+    x = float(input("Enter a number: "))
+    print(floor(x))
+elif(a==5):
+    x = float(input("Enter an angle in radians: "))
+    print(cos(x))
+elif(a==6):
+    x = float(input("Enter an angle in radians: "))
+    print(sin(x))
+elif(a==7):
+    x = float(input("Enter an angle in radians: "))
+    print(tan(x))
+elif(a==8):
+    x = float(input("Enter a number: "))
+    print(fabs(x))
+else:
+    print("Invalid choice")
     x=int(input)
